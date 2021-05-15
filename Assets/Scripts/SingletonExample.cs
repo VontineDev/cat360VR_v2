40f3f8ced972b5933357a5e4fd1a93738ac999b5
VontineDev <57723128+VontineDev@users.noreply.github.com>
2021-05-15 23:58:57 +0900

    싱글톤 예제

---
+++ Assets/Scripts/SingletonExample.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonExample : MonoBehaviour
+{
+    public static SingletonExample Instance;
+
+    private void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+    }
+
+    public void SingltoneMethod()
+    {
+        print($"Called singleton Method");
+    }
+
+
+}
