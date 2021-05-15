abc40668a06bd18c55a0f799d3f81146efa8e9e7
babodya <jeh016@naver.com>
2021-05-15 23:41:34 +0900

    eunhee-fade

---
+++ Assets/eunhee/Scripts/ChangeScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public GameObject sceneManagerObj;
+
+    public FadeManager FadeManager;
+
+    private void Start()
+    {
+        FadeManager = sceneManagerObj.GetComponent<FadeManager>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.CapsLock)){
+
+            FadeManager.FadeCoroutine();
+
+            SceneManager.LoadScene(1, LoadSceneMode.Additive);
+        }
+    }
+}
---
+++ Assets/eunhee/Scripts/FadeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeManager : MonoBehaviour
+{
+    public Image image;
+
+    void Start()
+    {
+        StartCoroutine(FadeCoroutine());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator FadeCoroutine()
+    {
+        float fadeCount = 1.0f;
+
+        while(fadeCount > 0f)
+        {
+            fadeCount -= 0.01f;
+            yield return new WaitForSeconds(0.01f);
+            image.color = new Color(0, 0, 0, fadeCount);
+        }
+    }
+}
