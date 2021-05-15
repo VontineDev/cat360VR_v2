8e8082a44d0b859e046f4fb342c1ee0cd8f2b4a1
babodya <jeh016@naver.com>
2021-05-15 17:49:28 +0900

    eunhee-soundManager

---
+++ Assets/eunhee/Scripts/SoundManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource miaow;
+
+    bool isMiaow;
+
+    private void Start()
+    {
+        isMiaow = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            miaowSound();
+        }
+        
+    }
+
+    void miaowSound()
+    {
+        if(isMiaow == false)
+        {
+            miaow.Play();
+
+            isMiaow = true;
+        }
+        else
+        {
+            miaow.Pause();
+
+            isMiaow = false;
+        }
+    }
+}
