1a445fe36a8601232f4e3e91156dc4eaf1a3c8ae
powerlsj7 <46619303+powerlsj7@users.noreply.github.com>
2021-05-15 21:50:10 +0900

    button
    button click

--- Assets/Scripts/TextA.cs
+++ Assets/Scripts/TextA.cs
@@ -17,15 +17,33 @@ public class TextA : MonoBehaviour
     private int idx=0;
     [Tooltip("줄간격 나눈 문장 출력 시간 간격")]
     public float time;
+    [Tooltip("버튼")]
+    public Button Yes;
+    [Tooltip("Enter로 줄간격 나눈 문장이 출력됩니다.10줄까지가능")]
+    [TextArea(1, 10)]
+    public string strYes;
+    [Tooltip("버튼")]
+    public Button No;
+    [Tooltip("Enter로 줄간격 나눈 문장이 출력됩니다.10줄까지가능")]
+    [TextArea(1, 10)]
+    public string strNo;
+    private IEnumerator corutine;
+   
     private void Start()
     {
+        corutine = textGo();
         idx = 0;
-      strArr =  str.Split('\n');
-        StartCoroutine(textGo());
+        SetTextStr(str);
+        StartCoroutine(corutine);
         for (int i = 0; i < 3; i++)
         {
             Debug.Log(strArr[i]);
         }
+    }
+    public void SetTextStr(string s)
+    {
+        idx = 0;
+        strArr = s.Split('\n');
     }
     IEnumerator textGo()
     {
@@ -33,11 +51,42 @@ public class TextA : MonoBehaviour
         text.text = "";
         text.DOText(strArr[idx], 2f, true).SetDelay(0f);
         idx++;
+      
         if(idx==strArr.Length)
         {
+            if(Yes!=null&&No!=null)
+            {
+            Invoke("SetActiveButton", 2f);
+            }
+            corutine = null;
             yield break;
         }
         StartCoroutine(textGo());
         yield break;
+    }
+    public void SetActiveButton()
+    {
+        Yes.gameObject.SetActive(true);
+        No.gameObject.SetActive(true);
+    }
+    public void YesButton()
+    {
+        if(corutine==null)
+        {
+            corutine = textGo();
+        SetTextStr(strYes);
+        StartCoroutine(corutine);
+        Debug.Log("들어옴");
+        }
+    }
+    public void NoButton()
+    {
+        if (corutine == null)
+        {
+            corutine = textGo();
+            SetTextStr(strNo);
+            StartCoroutine(corutine);
+            Debug.Log("들어옴");
+        }
     }
 }
