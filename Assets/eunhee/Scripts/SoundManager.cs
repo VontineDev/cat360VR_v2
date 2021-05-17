32edf69e54fa4047585e78d91d70623abad9f1f2
babodya <jeh016@naver.com>
2021-05-17 16:01:03 +0900

    eunhee-Create3DSound-update

--- Assets/eunhee/Scripts/SoundManager.cs
+++ Assets/eunhee/Scripts/SoundManager.cs
@@ -20,6 +20,10 @@ public class SoundManager : MonoBehaviour
     public AudioSource happyNyang;
 
 
+    // 3D Sound에 담을 Clip 파일
+    public AudioClip exexSound;
+
+
     private void Awake()
     {
         if (!Instance)
@@ -34,7 +38,7 @@ public class SoundManager : MonoBehaviour
         isHappyCat = false;
 
         bgm = GetComponent<AudioSource>();
-      //  bgm.Play();
+        //  bgm.Play();
     }
 
     public void miaowSound()
@@ -75,5 +79,51 @@ public class SoundManager : MonoBehaviour
 
             isHappyCat = true;
         }
+    }
+
+    // Sound Object 생성, 3D AudioSource 생성
+    public void Exex() { 
+    
+        // AudioSource를 담을 Sound 이름을 가진 Object 생성
+        GameObject soundObj = new GameObject("Sound");
+
+        // SoundObject 위치값 지정
+        soundObj.transform.position = new Vector3(1.400165f, -1.0f, -2.653213f);
+
+        // Sound Object 에 AudioSource Component를 생성 하여 넣어 준다.
+        AudioSource source = soundObj.AddComponent<AudioSource>();
+
+        // 100%의 Sound를 들을 수 있는 최소 거리 값.
+        source.minDistance = 0.5f;
+
+        // 50%의 Sound만 들을 수 있는 최대 거리 값.
+        source.maxDistance = 1.5f;
+
+        // AudioSourceCurveType의 SpatialBlend을 가지고 온다(Pan Level Custon Curve)를 가져옴
+        AnimationCurve ac = source.GetCustomCurve(AudioSourceCurveType.SpatialBlend);
+
+        // Curve의 KeyFrame을 담을 객체 생성
+        Keyframe[] keys = new Keyframe[1];
+
+        // KeyFrame 값을 1으로 변경.
+        for (int i = 0; i < keys.Length; ++i)
+        {
+            keys[i].value = 1f;
+        }
+
+        // Curve에 변경 된 키 값을 담아 준다.
+        ac.keys = keys;
+
+        // 3D sound로 변경
+        source.SetCustomCurve(AudioSourceCurveType.SpatialBlend, ac);
+
+        // AudioSource에 SoundClip 할당
+        source.clip = exexSound;
+
+        // Sound 반복 재생 0
+        source.loop = true;
+
+        // Sound 재생
+        source.Play();
     }
 }
