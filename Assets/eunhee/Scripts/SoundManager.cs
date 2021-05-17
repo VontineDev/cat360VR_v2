d9e7e6e54d7143ba2a39afdff0ff0824abf7358d
babodya <jeh016@naver.com>
2021-05-17 15:25:49 +0900

    eunhee-soundUpdate

--- Assets/eunhee/Scripts/SoundManager.cs
+++ Assets/eunhee/Scripts/SoundManager.cs
@@ -6,15 +6,20 @@ public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance;
 
-
-
     [SerializeField]
     AudioSource miaow;      //AudioSource of cat
 
     bool isMiaow;          //is cat crying?
 
+    bool isHappyCat;
+
     AudioSource bgm;       //AudioSource of bgm
+
     public AudioSource audioSource;
+
+    public AudioSource happyNyang;
+
+
     private void Awake()
     {
         if (!Instance)
@@ -25,6 +30,9 @@ public class SoundManager : MonoBehaviour
     private void Start()
     {
         isMiaow = false;
+
+        isHappyCat = false;
+
         bgm = GetComponent<AudioSource>();
       //  bgm.Play();
     }
@@ -47,5 +55,25 @@ public class SoundManager : MonoBehaviour
     public void PlaySound()
     {
         audioSource.Play();
+    }
+
+    public void happayNyangSound()
+    {
+        if(isHappyCat == false)
+        {
+            happyNyang.Play();
+
+            happyNyang.loop = true;
+
+            isHappyCat = false;
+        }
+        else
+        {
+            happyNyang.Pause();
+
+            happyNyang.loop = false;
+
+            isHappyCat = true;
+        }
     }
 }
